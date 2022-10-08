f6494ea28529d0c8183d2dd9d65a4017fe4482cc
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-10-08 20:49:07 +0200

    Added run scripts

--- go/main.go
+++ go/main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/thearyanahmed/libsandbox/binary_search"
 )
 
 func main() {
-	fmt.Println(binary_search.SquareRoot(4))
-	fmt.Println(binary_search.SquareRoot(8))
-	fmt.Println(binary_search.SquareRoot(1000))
+	fmt.Println("Hello")
 }
