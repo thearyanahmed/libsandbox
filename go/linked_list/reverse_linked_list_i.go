7ab88549e734caaa52a5cd3758a6b41584f1c7be
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-10-10 22:45:01 +0200

    added reverse linked list

---
+++ go/linked_list/reverse_linked_list_i.go
@@ -0,0 +1,14 @@
+package linked_list
+
+type ReverseLinkedListNode struct {
+	Val  int
+	Next *ReverseLinkedListNode
+}
+
+func ReverseLinkedList(head *ReverseLinkedListNode) *ReverseLinkedListNode {
+	var prev *ReverseLinkedListNode
+	for head != nil {
+		head.Next, prev, head = prev, head, head.Next
+	}
+	return prev
+}
